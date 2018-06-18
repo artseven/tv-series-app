f1dc7e1eb24f1808f7dfc57ec4ffcce5ef38c5f8
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-06-18 11:52:50 -0400

    series fully moved into own component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -5,15 +5,7 @@ import './App.css';
 import 'whatwg-fetch';
 
 class App extends Component {
-  state = {
-    series: []
-  }
 
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Supernatural')
-    .then((response) => response.json())
-    .then(json => this.setState({ series: json}));
-  }
 
   render() {
     return (
@@ -22,7 +14,6 @@ class App extends Component {
           <h1 className="App-title">TV Series List</h1>
         </header>
         <Intro message="Here you can find all of your most loved series"/>
-        The length of series array - {this.state.series.length}
         <Series />
       </div>
     );
