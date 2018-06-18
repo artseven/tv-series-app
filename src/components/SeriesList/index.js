6351349cd93278816fb57bf610e5430cd07860c0
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-06-18 12:51:02 -0400

    moving li into separate local component

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './index.css';
 
+const SeriesListItem = ({series}) => (
+    
+    <li key={series.show.id}>
+        {series.show.name}
+    </li>
+    
+)
+
 const SeriesList = (props) => {
     return (
         <div>
             <ul className="series-list">
                 {props.list.map(series => (
-                    <li>{series.show.name}</li>
+                    <SeriesListItem series={series}/>
                 ))}
             </ul>
         </div>
