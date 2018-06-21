b523db0b66a3cf0f166daee17164efcefa543b37
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-06-21 15:26:50 -0400

    style improvements

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -4,6 +4,7 @@ import LoaderSrc from '../../assets/loader.gif';
 const Loader = props => (
     <div>
         <img
+         style={{ width: 75 }}
          alt="Loader icon"
          src={LoaderSrc} />
     </div>
