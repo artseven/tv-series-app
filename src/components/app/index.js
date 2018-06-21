52d1250cf78ba622c08b681131dba0370a9a7a7c
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2018-06-21 15:50:13 -0400

    refactoring

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Intro from '../Intro';
-import Series from '../../containers/Series';
+import Main from '../Main';
 import './App.css';
 import 'whatwg-fetch';
 
@@ -13,7 +12,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">TV Series List</h1>
         </header>
-        <Series />
+        <Main />
       </div>
     );
   }
